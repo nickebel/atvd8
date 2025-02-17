cd3666f082545f7fc81308ec81d852e097136fac
nickebel <nas20@aluno.ifal.edu.br>
2025-02-17 08:29:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function exibir() {
+    let nome = document.getElementById("txtNome").value;
+    let cidade = document.getElementById("cidade").value;
+    let opcoes = document.getElementsByName("opcoes");
+    let valorSelecionado = "";
+
+   
+
+    alert( `Bem-vindo {nome}, a cidade escolhida foi . Você mora na cidade: valorSelecionado.`);
+ }
